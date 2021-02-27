8fd64a3abe5b70bbc0f361a87f086a6c0fff47c1
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-02-27 01:09:04 +0100

    maj de la page d'acceuil

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Acceuil</title>
+    <link rel="stylesheet" href="styles/template.css" />
+    <link rel="stylesheet" href="styles/acceuil.css" />
+</head>
+
+<body>
+
+    <?php
+    include "templates/header.html"; ?>
+
+    <div id="acceuil">
+        <img src="images/photo_bureau.png" alt="bureau" />
+        <p>Pirates des Carabed</p>
+    </div>
+
+    <?php
+    include "templates/footer.html";
+    ?>
+
+</body>
+</html>
