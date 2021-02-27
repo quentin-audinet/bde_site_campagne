fafd3bf7d791ea1c4fe0668e6df0ce9eb74ea705
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-02-27 12:29:40 +0100

    version anglaise non traduite

---
+++ en/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <title>Acceuil</title>
+    <link rel="stylesheet" href="../styles/template.css" />
+    <link rel="stylesheet" href="../styles/acceuil.css" />
+    <script>
+        var page = "index.php";
+    </script>
+</head>
+
+<body>
+
+    <?php
+    include "../templates/header.html"; ?>
+
+    <div id="acceuil">
+        <img src="../images/photo_bureau.png" alt="bureau" />
+        <p>Pirates des Carabed</p>
+    </div>
+
+    <?php
+    include "../templates/footer.html";
+    ?>
+
+</body>
+</html>
