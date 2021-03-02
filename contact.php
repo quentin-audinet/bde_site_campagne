bbc979c0acb233eb61df794f7778630fe1b96cbc
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-03-02 02:31:17 +0100

    Carte avec effet zoom

--- contact.php
+++ contact.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8" />
     <title class="t_contact">Nous contacter</title>
     <link rel="stylesheet" href="styles/template.css" />
+    <link rel="stylesheet" href="styles/contact.css" />
+    <script type="text/javascript" src="scripts/jquery-1.5.2.min.js"></script>
+    <script type="text/javascript" src="scripts/map_zoom.js"></script>
 </head>
 
 <body>
 
 <?php include "templates/header.php";?>
 
+<div class="map_container">
+    <canvas id="map_panel" width="770" height="513"></canvas>
+</div>
 
 <?php include "templates/footer.html"; ?>
 </body>
---
+++ scripts/map_zoom.js
@@ -0,0 +1,52 @@
+var canvas, ctx;
+var image;
+var iMouseX, iMouseY = 1;
+var bMouseDown = false;
+var iZoomRadius = 100;
+var iZoomPower = 4;
+
+const clear = () => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+const drawScene = () => {
+    clear();
+    if(bMouseDown) {
+        ctx.drawImage(image, 0 - iMouseX * (iZoomPower - 1), 0 - iMouseY * (iZoomPower - 1),
+            ctx.canvas.width * iZoomPower, ctx.canvas.height * iZoomPower);
+        ctx.globalCompositeOperation = 'destination-atop';
+
+        var oGrd = ctx.createRadialGradient(iMouseX, iMouseY, 0, iMouseX, iMouseY, iZoomRadius);
+        oGrd.addColorStop(0.8, "rgba(0, 0, 0, 1.0)");
+        oGrd.addColorStop(1.0, "rgba(0, 0, 0, 0.1)");
+        ctx.fillStyle = oGrd;
+        ctx.beginPath();
+        ctx.arc(iMouseX, iMouseY, iZoomRadius, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+    ctx.drawImage(image, 0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+$(function(){
+    image = new Image();
+    image.onload = function () {
+    }
+    image.src = "images/carte.jpg";
+
+    canvas = document.getElementById('map_panel');
+    ctx = canvas.getContext('2d');
+    $('#map_panel').mousemove(function (e){
+        var canvasOffset = $(canvas).offset();
+        iMouseX = Math.floor(e.pageX - canvasOffset.left);
+        iMouseY = Math.floor(e.pageY - canvasOffset.top);
+    });
+    $('#map_panel').mousedown(function (e) {
+        bMouseDown = true;
+    });
+    $('#map_panel').mouseup(function (e) {
+        bMouseDown = false;
+    });
+    setInterval(drawScene, 30);
+});
