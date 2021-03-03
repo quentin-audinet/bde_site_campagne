e8210fefa77a55d1b0d2e407fad30d11a270bff9
Pascal Rouppert <pascal.rouppert@netc.eu>
2021-03-03 10:51:04 +0100

    Test

---
+++ Galerie.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style_galerie.css" />
+        <link rel="stylesheet" href="styles/template.css" />
+	</head>
+
+    <body>
+        <?php include "templates/header.php"; ?>
+        <?php include "templates/footer.hmtl"; ?>
+    </body>
+
+
+
+</html>
+
