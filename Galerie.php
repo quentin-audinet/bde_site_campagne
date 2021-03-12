427086c21f58e541b6bf888fce5cfec7ceacbbfb
Pascal Rouppert <pascal.rouppert@netc.eu>
2021-03-12 13:15:11 +0100

    Version propre de la galerie de photos

--- Galerie.php
+++ Galerie.php
@@ -1,19 +1,36 @@
-
 <!DOCTYPE html>
-<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="styles/galery.css" />
+    <link rel="stylesheet" href="styles/template.css" />
+    <title>Galerie photo</title>
+</head>
 
-    <head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="style_galerie.css" />
-        <link rel="stylesheet" href="styles/template.css" />
-	</head>
+<body>
+    <?php include "templates/header.php";?>
+    <div id="images_zone">
+        <div class="image_div">
+            <img class = "image" src="Galerie_photos/Bateau.jpg"/>
+            <div class="description">Aujourd'hui, Jack Sparrow mange son casse-croûte</div>
+        </div>
+        <div class="image_div">
+            <img class = "image" src="Galerie_photos/Boussole.jpeg"/>
+            <div class="description">Aujourd'hui, Jack Sparrow mange son casse-croûte</div>
+        </div>
+        <div class="image_div">
+            <img class = "image" src="Galerie_photos/Jack_sparrow.jpg"/>
+            <div class="description">Aujourd'hui, Jack Sparrow mange son casse-croûte</div>
+        </div>
+        <div class="image_div">
+            <img class = "image" src="Galerie_photos/Photo_famille.jpg"/>
+            <div class="description">Aujourd'hui, Jack Sparrow mange son casse-croûte</div>
+        </div>
+        <div class="image_div">
+            <img class = "image" src="Galerie_photos/Poupee_vaudou.jpg"/>
+            <div class="description">Aujourd'hui, Jack Sparrow mange son casse-croûte</div>
+        </div>
+    </div>
+    <?php include "templates/footer.html";?>
+</body>
 
-    <body>
-        <?php include "templates/header.php"; ?>
-        <?php include "templates/footer.hmtl"; ?>
-    </body>
-
-
-
-</html>
 
