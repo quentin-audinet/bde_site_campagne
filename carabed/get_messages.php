fdbe4ba7bae6ee4695d3fcbaaf655ceb1528ccce
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-03-12 23:25:20 +0100

    script pour envoyer un message

---
+++ carabed/get_messages.php
@@ -0,0 +1,11 @@
+<?php
+function getLastMessages() {
+    require "../db/db_connect.php";
+    return $db->query("SELECT username, message, date FROM chat JOIN users ON user_id=users.id ORDER BY `date` DESC LIMIT 10");
+}
+
+$messages = getLastMessages();
+while($row = $messages->fetch()) {
+    print("<b>".$row['username'] . "</b> >> " . $row['message'] . "<br/>");
+}
+?>
