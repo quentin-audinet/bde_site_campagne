052a3a4314e28e726eb3bb2b0e217884af80e39c
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-03-13 01:14:04 +0100

    mise en place du chat

---
+++ carabed/chat.php
@@ -0,0 +1,68 @@
+<?php
+
+function sendMessage($message)
+{
+    if(!empty($message)) {
+        if(empty($_SESSION)) {
+            require "conf.php";
+        }
+        $req = $db->prepare("INSERT INTO chat (`user_id`, `message`) VALUES (:id, :message)");
+        $req->execute(array(
+        'id' => $_SESSION['id'],
+        'message' => $message
+        ));
+    }
+}
+
+if(isset($_POST['message'])) {
+    sendMessage($_POST['message']);
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Chat</title>
+    <link rel="stylesheet" href="styles/chat.css" />
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+    <script>
+        window.onload = function () {
+            const chat = document.getElementById("container");
+            chat.scrollTop = chat.scrollHeight;
+        }
+    </script>
+</head>
+
+<body>
+
+<?php include "header.html"; ?>
+<div id="container">
+    <?php
+    require "conf.php";
+    $messages = $db->query("SELECT username, message, `date` FROM chat JOIN users ON user_id=users.id ORDER BY `date` ASC");
+    while($row = $messages->fetch()) {
+        $class = $row['username']==$_SESSION['username']?"user":"other";
+        $date = $row['date'];
+        $username = $row['username'];
+        $message = $class=="other"?">> ".$row['message']:$row['message']." <<";
+        print("
+<div class='$class message'>
+<span class='username'>$username</span><br/>
+<span class='content'>$message</span><br/>
+<span class='date'>$date</span>
+</div>");
+    }
+    ?>
+</div>
+
+<form action="" method="post">
+    <input id="message" name="message" type="text" /><button style="background: none;border: none"><i class="fa fa-paper-plane fa-2x" aria-hidden="true"></i></button>
+</form>
+
+</body>
+</html>
