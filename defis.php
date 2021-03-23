232093f0b3fe3b7420a429c052ded68a7c55156b
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-03-23 18:31:03 +0100

    redirection vers leurs défis

--- defis.php
+++ defis.php
@@ -16,6 +16,11 @@
             height: 800px
         }
 
+        .logo-liste {
+            align-self: center;
+            margin: auto;
+        }
+
         #sabres {
             padding-top: 50px;
             position: relative;
@@ -43,16 +48,19 @@
 <?php include "templates/header.php";?>
 
 <div id="container">
+    <img class="logo-liste" src="images/telecom_paristech.jpg" height="200"/>
     <div id="sabres">
         <img id="sabre-autres" src="images/sabre_autres_min.png"/>
 
         <img id="sabre-carabed" src="images/sabre_carabed_min.png" usemap="#image-map-min-carabed">
         <map id="map-carabed" name="image-map-min-carabed">
             <area id="area-carabed" target="_self" alt="" title="" href="carabed_defis.php" coords="97,543,134,505,175,477,225,444,192,391,195,372,212,366,243,367,291,408,402,300,469,193,512,110,558,118,611,98,681,52,671,99,627,193,568,272,468,361,370,428,322,455,333,512,324,577,287,626,231,653,154,640,103,586,94,608,69,620,43,608,36,589,44,563,61,552,86,555,76,519,84,518" shape="poly">
-            <area id="area-autres" target="_self" alt="" title="" href="#" coords="603,588,556,636,511,653,457,649,406,619,376,565,373,501,383,456,230,359,112,246,52,147,22,54,94,104,154,118,193,112,251,224,327,324,414,409,451,377,496,365,513,377,509,401,480,446,540,483,610,544,621,525,627,519,628,535,621,556,643,554,659,561,671,588,659,614,634,622,611,612" shape="poly">
+            <area id="area-autres" target="_self" alt="" title="" href="warning.html" coords="603,588,556,636,511,653,457,649,406,619,376,565,373,501,383,456,230,359,112,246,52,147,22,54,94,104,154,118,193,112,251,224,327,324,414,409,451,377,496,365,513,377,509,401,480,446,540,483,610,544,621,525,627,519,628,535,621,556,643,554,659,561,671,588,659,614,634,622,611,612" shape="poly">
         </map>
 
     </div>
+    <img class="logo-liste" src="images/telecom_paristech.jpg" height="200"/>
+
 </div>
 
 
@@ -79,6 +87,10 @@
         image_autres.setAttribute("src","images/sabre_autres_min.png");
         area_autres.setAttribute("coords","603,588,556,636,511,653,457,649,406,619,376,565,373,501,383,456,230,359,112,246,52,147,22,54,94,104,154,118,193,112,251,224,327,324,414,409,451,377,496,365,513,377,509,401,480,446,540,483,610,544,621,525,627,519,628,535,621,556,643,554,659,561,671,588,659,614,634,622,611,612");
     });
+
+    area_autres.addEventListener("click", () => {
+        alert("Es-tu sur ?");
+    })
 
 
 </script>
