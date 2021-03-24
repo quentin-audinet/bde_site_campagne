1f36192c3724bfc72d3d4dbb8b4776f32f94bb26
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-03-24 09:10:33 +0100

    corrections et mise en page agenda

--- rgnpd.php
+++ rgnpd.php
@@ -29,8 +29,8 @@
 
     <p>
         En visitant ce site tu t'engages à ne pas essayer de le hacker, ça me mettrait de mauvaise humeur. Tu t'engages aussi à ne pas porter plainte en cas de fuite "accidentelle" de tes coordonnées bancaires sur le darknet.
-        Si tu venais à découvrir un problème de sécurité majeur, comme tu es quelqu'un de bien élevé, tu t'engages à ne pas exploiter cette faille et à contacter un webmaster au plus vite (Quentin Audinet ou Pascal Roupert).</p>
-    <p>Tu t'engages aussi à connaître le code ASCII au cas où tu trouverais un malencontreux fichier perdu sur ce site codé avec des nombres bizzares qui te rappelleraient vaguement tes cours d'INF104.</p>
+        Si tu venais à découvrir un problème de sécurité majeur, comme tu es quelqu'un de bien élevé, tu t'engages à ne pas exploiter cette faille et à contacter un webmaster au plus vite (Quentin Audinet ou Pascal Rouppert).</p>
+    <p>Tu t'engages aussi à connaître le code ASCII au cas où tu trouverais un malencontreux fichier perdu sur ce site, codé avec des nombres bizzares qui te rappelleraient vaguement tes cours d'INF104.</p>
     <p>Le plus évident, tu t'engages à soutenir Pirates des Carabed, quitte à y laisser ta peau. Tu t'engages aussi à mettre à disposition ton ordinateur lors d'une attaque bruteforce massive sur Mewteub.
         Tu t'engages à offir un cookie à Quentin Audinet qui est actuellement en train de se faire chier à écrire un foutu règlement général sur la (non)protection de tes données.
         Et enfin, et de loin le plus important, tu t'engages à kiffer un max lors de la campagne !
@@ -38,7 +38,7 @@
 
     <h3>[English version]</h3>
     <p>By visiting this site you agree not to try to hack it, it would put me in a bad mood. You also agree not to file a complaint in the event of an "accidental" leak of your bank details on the darknet.
-        If you were to discover a major security problem, as you are a well-educated person, you agree not to exploit this flaw and to contact a webmaster as soon as possible (Quentin Audinet or Pascal Roupert).</p>
+        If you were to discover a major security problem, as you are a well-educated person, you agree not to exploit this flaw and to contact a webmaster as soon as possible (Quentin Audinet or Pascal Rouppert).</p>
 
     <p>You also agree to know the ASCII code in case you find an unfortunate lost file on this site encoded with strange numbers that vaguely remind you of your INF104 lessons.</p>
 
@@ -49,7 +49,7 @@
     <h3>[Versión en Español]</h3>
         <p>
         Al visitar este sitio, aceptas no intentar piratearlo, me pondría de mal humor. También acepta no presentar una queja en caso de una filtración "accidental" de sus datos bancarios en la darknet.
-        Si descubriera un problema de seguridad importante, ya que es una persona bien educada, acepta no explotar este defecto y ponerse en contacto con un webmaster lo antes posible (Quentin Audinet o Pascal Roupert). </ P>
+        Si descubriera un problema de seguridad importante, ya que es una persona bien educada, acepta no explotar este defecto y ponerse en contacto con un webmaster lo antes posible (Quentin Audinet o Pascal Rouppert). </ P>
     <p> También acepta conocer el código ASCII en caso de que encuentre un archivo perdido desafortunado en este sitio codificado con números extraños que le recuerden vagamente sus lecciones INF104. </p>
     <p> Lo más obvio es que te comprometes a apoyar a Pirates of the Carabed, incluso si eso significa dejar tu piel allí. También acepta que su computadora esté disponible durante un ataque masivo de fuerza bruta en Mewteub.
         Usted acepta ofrecer una cookie a Quentin Audinet, quien actualmente está enojado escribiendo un maldito reglamento general sobre la (no) protección de sus datos.
@@ -60,7 +60,7 @@
 
     <p>
         Wenn Sie diese Seite besuchen, stimmen Sie zu, nicht zu versuchen, sie zu hacken, da dies mich in schlechte Laune versetzen würde. Sie erklären sich auch damit einverstanden, keine Beschwerde einzureichen, falls Ihre Bankdaten im Darknet "versehentlich" verloren gehen.
-        Wenn Sie als gut ausgebildete Person ein schwerwiegendes Sicherheitsproblem entdecken, erklären Sie sich damit einverstanden, diesen Fehler nicht auszunutzen und sich so schnell wie möglich an einen Webmaster (Quentin Audinet oder Pascal Roupert) zu wenden. </ P>
+        Wenn Sie als gut ausgebildete Person ein schwerwiegendes Sicherheitsproblem entdecken, erklären Sie sich damit einverstanden, diesen Fehler nicht auszunutzen und sich so schnell wie möglich an einen Webmaster (Quentin Audinet oder Pascal Rouppert) zu wenden. </ P>
     <p> Sie erklären sich auch damit einverstanden, den ASCII-Code zu kennen, falls Sie auf dieser Site eine unglückliche verlorene Datei finden, die mit seltsamen Zahlen codiert ist, die Sie vage an Ihre INF104-Lektionen erinnern. </ p>
     <p> Am offensichtlichsten ist, dass Sie sich dazu verpflichten, Pirates of the Carabed zu unterstützen, auch wenn dies bedeutet, dass Sie Ihre Haut dort lassen. Sie stimmen auch zu, Ihren Computer während eines massiven Bruteforce-Angriffs auf Mewteub zur Verfügung zu stellen.
         Sie erklären sich damit einverstanden, Quentin Audinet ein Cookie anzubieten, der derzeit sauer ist, eine verdammt allgemeine Regelung zum (Nicht-) Schutz Ihrer Daten zu schreiben.
@@ -71,7 +71,7 @@
 
     <p>
         通过访问该网站，您同意不尝试对其进行破解，这会让我很沮丧。您还同意在Darknet上银行信息“意外”泄露的情况下不提出投诉。
-        如果您是一个受过良好教育的人，那么如果您发现一个重大的安全问题，则表示您同意不利用此漏洞并尽快与网站管理员联系（Quentin Audinet或Pascal Roupert）。</ P>
+        如果您是一个受过良好教育的人，那么如果您发现一个重大的安全问题，则表示您同意不利用此漏洞并尽快与网站管理员联系（Quentin Audinet或Pascal Rouppert）。</ P>
     <p>您还同意知道ASCII码，以防您在本网站上发现一个不幸的丢失文件，并用奇怪的数字编码，从而使您想起INF104课程。</ p>
     <p>最明显的是，您承诺支持Carabed海盗，即使这意味着将皮肤留在那儿。您还同意在对Mewteub进行大规模暴力攻击期间使计算机可用。
         您同意向Quentin Audinet提供cookie，后者目前正在为关于（非）保护数据的糟糕的一般法规而烦恼。
@@ -82,7 +82,7 @@
     <h3>[日本語版]</h3>
     <p>
         このサイトにアクセスすることで、ハッキングを試みないことに同意したことになり、機嫌が悪くなります。また、ダークネットで銀行の詳細が「偶発的に」漏洩した場合でも、苦情を申し立てないことに同意するものとします。
-        十分な教育を受けた人であるため、重大なセキュリティ問題を発見した場合は、この欠陥を悪用せず、できるだけ早くWebマスター（QuentinAudinetまたはPascalRoupert）に連絡することに同意するものとします。</ P>
+        十分な教育を受けた人であるため、重大なセキュリティ問題を発見した場合は、この欠陥を悪用せず、できるだけ早くWebマスター（QuentinAudinetまたはPascalRouppert）に連絡することに同意するものとします。</ P>
     <p>また、INF104のレッスンを漠然と思い出させる奇妙な数字でエンコードされた、このサイトで不幸な紛失ファイルを見つけた場合に備えて、ASCIIコードを知っていることに同意します。</ p>
     <p>最も明白なことは、たとえそれがあなたの肌をそこに残すことを意味するとしても、あなたはパイレーツオブカラベドを支援することを約束します。また、Mewteubへの大規模なブルートフォース攻撃中にコンピューターを使用可能にすることに同意します。
         あなたは、あなたのデータの（非）保護に関するひどい一般的な規制を書くことに現在腹を立てているQuentinAudinetにCookieを提供することに同意します。
@@ -103,7 +103,7 @@
     <h3>[versio Latina]</h3>
     <P>
         Per hanc visitare situs conveniunt ad trucidabunt non tentant illum, et pone me ut malus modus. Tu quoque conveniunt, ne de eventu esse file a querimoniam ad 'per accidens' tuæ exitum tuum super ripam singula darknet.
-        Si pignus a major esset ad invenire problema ut sit bene et liberaliter educatus sis homo, et in vobis ut non facias violentiam hoc vitium est Magister aranearius contact quam primum (Parvis aut uni Audinet Roupert). </ P>
+        Si pignus a major esset ad invenire problema ut sit bene et liberaliter educatus sis homo, et in vobis ut non facias violentiam hoc vitium est Magister aranearius contact quam primum (Parvis aut uni Audinet Rouppert). </ P>
     <P> Et conveniunt et scire ASCII codice si habes infortunatus perditus factus est encoded lima in hoc situm est, quod quadam confusa cognitione, totiens tibi tuarum INF104 Lectiones numeri novis. </ P>
     <P> In promptu sit, ut te committere supporting Carabed Piratae, etiam si vestri cutis est relinquens est. Et conveniunt ad te praesto computatrum tuum in bruteforce ingens impetum in Mewteub.
         Et conveniunt ad Quintini Audinet qui currently offerre crustulum meientis regula off in scribo Damnare (non) tutela vestri notitia.
@@ -113,7 +113,7 @@
     <h3>[Esperanta versio]</h3>
     <p>
         Vizitante ĉi tiun retejon vi konsentas ne provi haki ĝin, tio malbonhumorigus min. Vi ankaŭ konsentas ne plendi kaze de "hazarda" likado de viaj bankaj detaloj sur la malluma reto.
-        Se vi malkovrus gravan sekurecan problemon, ĉar vi estas altedukita homo, vi konsentas ne ekspluati ĉi tiun difekton kaj kontakti retejon kiel eble plej baldaŭ (Quentin Audinet aŭ Pascal Roupert). </ P>
+        Se vi malkovrus gravan sekurecan problemon, ĉar vi estas altedukita homo, vi konsentas ne ekspluati ĉi tiun difekton kaj kontakti retejon kiel eble plej baldaŭ (Quentin Audinet aŭ Pascal Rouppert). </ P>
     <p> Vi ankaŭ konsentas koni la kodon ASCII, se vi trovos malfeliĉan perditan dosieron en ĉi tiu retejo koditan per strangaj nombroj, kiuj malklare memorigas vin pri viaj INF104-lecionoj. </p>
     <p> La plej evidenta, vi kompromitas subteni Piratojn de la Karabedoj, eĉ se tio signifas lasi vian haŭton tie. Vi ankaŭ konsentas disponigi vian komputilon dum amasa krudforta atako kontraŭ Mewteub.
         Vi konsentas oferti kuketon al Quentin Audinet, kiu nuntempe koleras skribi diablan ĝeneralan reguladon pri la (ne) protekto de viaj datumoj.
