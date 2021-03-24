02306c997e61fcef4474c9ac61484c8ddf3e8984
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-03-24 09:09:50 +0100

    page de news

--- news.php
+++ news.php
@@ -5,13 +5,30 @@
 <head>
     <meta charset="UTF-8" />
     <title class="t_news">News</title>
+    <link rel="stylesheet" href="styles/news.css" />
     <link rel="stylesheet" href="styles/template.css" />
 </head>
 
 <body>
 <?php include "templates/header.php";?>
 
-<h1>Toutes les news des Carabed</h1>
+<h1 class="title">Toutes les news des Carabed</h1>
+
+<?php
+include "db/db_connect.php";
+
+$data = $db->query("SELECT * FROM news ORDER BY date DESC, id DESC");
+while($row = $data->fetch()) {
+    $author = $row['auteur'];
+    $content = $row['contenu'];
+    $date = $row['date'];
+    print("
+    <h3 class='header'>Le $date de $author</h3>
+    <div class='news'>$content</div>
+    ");
+}
+
+?>
 
 <?php include "templates/footer.html";?>
 
