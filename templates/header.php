b875affcd67ef4d37b962e90f1bbf2bb6809c840
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-03-27 00:43:54 +0100

    media queries

--- templates/header.php
+++ templates/header.php
@@ -10,21 +10,32 @@
 </script>
 
 <header id="banner">
+    <nav>
         <ul>
-            <li><a href="index.php"><span class="t_acceuil" /></a></li>
-            <li><a href="news.php"><span class="t_news" /></a> </li>
-            <li><a href="Agenda.php"><span class="t_agenda" /></a></li>
-            <li><a href="defis.php"><span class="t_defis" /></a> </li>
-            <li><a href="Listeux.php"><span class="t_membres" /></a> </li>
-            <li><a href="partners.php"><span class="t_sponsors" /></a> </li>
-            <li><a href="Galerie.php"><span class="t_photos" /></a> </li>
-            <li><a href="contact.php"><span class="t_contact" /></a> </li>
-            <?php if(isset($_COOKIE['user_id'])) {
-                print("<li><a href='carabed/backside.php'>Backside</a></li>");
-            }?>
+            <a href="#banner" id="open">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="102px" height="96px" viewBox="0 0 102 100" enable-background="new 0 0 102 100" xml:space="preserve">
+                    <rect y=0 fill="#FFFFFF" width="102" height="12"/>
+                    <rect y=32 fill="#FFFFFF" width="102" height="12"/>
+                    <rect y=64 fill="#FFFFFF" width="102" height="12"/>
+                </svg>
+            </a>
+            <a href="#" id="close">x</a>
+            <div id="menu-items">
+                <li><a href="index.php" class="menu"><span class="t_acceuil" /></a></li>
+                <li><a href="Agenda.php" class="menu"><span class="t_agenda" /></a></li>
+                <li><a href="defis.php" class="menu"><span class="t_defis" /></a> </li>
+                <li><a href="Listeux.php" class="menu"><span class="t_membres" /></a> </li>
+                <li><a href="partners.php"class="menu"><span class="t_sponsors" /></a> </li>
+                <li><a href="Galerie.php" class="menu"><span class="t_photos" /></a> </li>
+                <li><a href="contact.php" class="menu"><span class="t_contact" /></a> </li>
+                <?php if(isset($_COOKIE['user_id'])) {
+                    print("<li><a href='carabed/backside.php' class='menu'>Backside</a></li>");
+                }?>
+            </div>
+
 
             <script>
-                for(let link of document.querySelectorAll("#banner ul li a")) {
+                for(let link of document.querySelectorAll("#banner ul #menu-items li a")) {
                     link.addEventListener("click", () => {
                         link.setAttribute("href",link.getAttribute("href")+"?lang="+lang);
                     });
@@ -32,6 +43,7 @@
             </script>
         </ul>
 
+        <div id="menu_bar"></div>
         <div id="language_selector" class="dropdown">
             <button onclick="chgLanguage()" class="dropbtn">▼&ensp;<img class="flag_menu" src="images/fr_flag.png" alt="French" /></button>
             <div id="dropdown_language" class="dropdown_content">
@@ -68,6 +80,7 @@
                 })
             }
         </script>
+    </nav>
 </header>
 
 <section class="content">
