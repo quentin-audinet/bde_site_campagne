ac311993736d95a82b707a06fd1ea1408b4d6d42
Pascal Rouppert <pascal.rouppert@netc.eu>
2021-04-21 23:07:05 +0200

    Bonsoir

---
+++ Babar/main.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>Le blog de babar</title>
+	</head>
+
+    <body>
+        <h1 id="main-title">Le site du bar</h1>
+        <ul id="beers" class="display-box">
+            <span id="beers-title-box" class="box-title">
+                <h1>Nos bières</h1>
+            </span>
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span id="beers-title-box" class="box-title">
+                <h1>Nos bières</h1>
+            </span>
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+        </ul>
+
+        <ul id="events" class="display-box">
+            <span id="beers-title-box" class="box-title">
+                <h1>Nos prochains events</h1>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+
+            <span class="in-display-box">
+                <span class="title">La Schouf</span>
+                <img class="image" src="Images/external-content.duckduckgo.com.jpeg" width = "200" height="200">
+                <p class="descr">Bonjour</p>
+                <p class="price-date">Bonjour</p>
+            </span>
+        </ul>
+    </body>
+
+
+
+
+
